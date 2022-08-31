142e1f75fcd9db2920932b4cfc5f3ee4f4764c06
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2022-08-31 18:34:20 -0300

    adicionado json

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 
 	"github.com/Laizacatafesta/API-Rest-personalidades.git/routes"
+	"github.com/Laizacatafesta/API-Rest-personalidades.git/routes/models"
 )
 
 func main() {
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Anne", Historia: "Historia 1"},
+		{Nome: "Diana", Historia: "Historia 2"},
+	}
+
 	fmt.Println("Iniciando")
 	routes.HandleRequest()
 }
